90a1a9b6cd613d2da04556b8e5d48fe018283463
subramanillrs <57629442+subramanillrs@users.noreply.github.com>
2026-08-27 12:38:40 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='banking-todo-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+const CACHE='banking-todo-v4';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.json']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
